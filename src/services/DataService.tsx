987789159d465dada6ddab66f36a3c130ac7ee6f
Barendan <barendan@gmail.com>
2022-05-16 16:57:06 -0400

    make history util and add crud methods to handle spaces

--- src/services/DataService.tsx
+++ src/services/DataService.tsx
@@ -1,3 +1,4 @@
+import { ICreateSpaceState } from "../components/spaces/CreateSpace";
 import { Reservation, ReservationState, Space, User } from "../model/Model";
 import { S3, config } from 'aws-sdk';
 import { config as appConfig} from './config';
@@ -56,6 +57,62 @@ export class DataService {
         return uploadResult.Location
     }
 
+    public async createSpace(iCreateSpace: ICreateSpaceState){
+        if (iCreateSpace.photo) {
+            const photoUrl = await this.uploadPublicFile(
+                iCreateSpace.photo,
+                appConfig.SPACES_PHOTOS_BUCKET
+            )
+            iCreateSpace.photoURL = photoUrl;
+            iCreateSpace.photo = undefined
+        }
+        const requestUrl = appConfig.api.spacesUrl;
+        const requestOptions: RequestInit = {
+            method: 'POST',
+            body: JSON.stringify(iCreateSpace)
+        }
+        const result = await fetch(requestUrl, requestOptions);
+        const resultJSON = await result.json();
+
+        return JSON.stringify(resultJSON.id);
+    }
+
+    public async getSpaces(): Promise<Space[]> {
+        if (this.user) {
+            console.log(`Using token: ${this.getUserIdToken()}`)
+            const requestUrl = appConfig.api.spacesUrl;
+            const requestResult = await fetch(
+                requestUrl, {
+                    method: 'GET',
+                    headers: {
+                        'Authorization': this.getUserIdToken()
+                    }
+                }
+            );
+            const responseJSON = await requestResult.json();
+            return responseJSON;
+        } else {
+            return []
+        }
+    }
+
+    public async reserveSpace(spaceId: string):Promise<string | undefined> {
+        const requestUrl = appConfig.api.reservationsUrl;
+        const requestResult = await fetch(
+            requestUrl, {
+                method: 'POST',
+                headers: {
+                    'Authorization': this.getUserIdToken()
+                }, 
+                body: JSON.stringify({
+                    spaceId: spaceId,
+                    user: this.user?.userName
+                })
+            }
+        );
+        const responseJSON = await requestResult.json();
+        return responseJSON.id;
+    }
 
 
 }
