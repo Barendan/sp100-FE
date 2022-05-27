398a15f2157ed22a99025d0dbc89b42753253101
Barendan <barendan@gmail.com>
2022-05-27 19:03:28 -0400

    add login and logout functionality

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,6 +7,13 @@ export class Navbar extends React.Component<{
 }> {
 
     render() {
+        let loginLogOut: any
+        if (this.props.user) {
+            loginLogOut = <Link to='/logout' style={{ float: 'right' }} >{this.props.user.userName} Logout</Link>
+        } else {
+            loginLogOut = <Link data-testid='login-link'  to='/login' style={{ float: 'right' }}>Login</Link>
+        }
+
 
         return (
             <div className='navbar'>
@@ -14,6 +21,7 @@ export class Navbar extends React.Component<{
                 <Link data-testid='profile-link' to='/profile'> Profile</Link>
                 <Link data-testid='spaces-link' to='/spaces'> Spaces</Link>
                 <Link to='/reservations'> Reservations</Link>
+                {loginLogOut}
             </div>
         )
     }
